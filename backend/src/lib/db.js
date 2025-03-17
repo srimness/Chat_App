0a7906e1a89987685dbfb78f471282aaf7388927
Vivek Karthikeyan <vivekkarthikeyan@Viveks-MacBook-Pro.local>
2025-03-17 23:08:25 +0530

    db timeout changes

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -3,8 +3,14 @@ import mongoose from 'mongoose';
 export const connectDB = async () => {
 
     try {
-        const connect = await mongoose.connect(process.env.MONGODB_URI);
+        const connect = await mongoose.connect(process.env.MONGODB_URI, {
+        useNewUrlParser: true,        // Ensure compatibility with MongoDB
+            useUnifiedTopology: true,    // Use the new unified topology engine
+            serverSelectionTimeoutMS: 30000,  // 30 seconds timeout for selecting servers
+            socketTimeoutMS: 45000, 
+        });
     console.log(`MongoDB Connected: ${connect.connection.host}`);
+
     
         
     } catch (error) {
